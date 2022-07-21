06906d7b4a7bd584f9bc59a466a6a091842ac04b
mickaelaustoni <mickael.austoni@gmail.com>
2022-07-21 11:57:31 +0200

    Base vite export component

---
+++ src/lib/components/Button.tsx
@@ -0,0 +1,5 @@
+const Button = ()=>{
+    return (<button>xxxx</button>)
+}
+
+export default Button;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import path from 'node:path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/lib/index.ts'),
+      name: 'design-system',
+      formats: ['es', 'umd'],
+      fileName: (format) => `design-system.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+});
