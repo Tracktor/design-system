2118f47c40ddaecea68673b90401ca2482981a3f
mickaelaustoni <mickael.austoni@gmail.com>
2022-07-22 14:32:13 +0200

    config

--- vite.config.ts
+++ vite.config.ts
@@ -8,15 +8,29 @@ export default defineConfig({
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"),
       fileName: (format) => `design-system.${format}.js`,
-      formats: ["es", "umd"],
+      formats: ["es"],
       name: "design-system",
     },
     rollupOptions: {
       external: ["react", "react-dom"],
+      input: {
+        Accordion: "src/components/Accordion.ts",
+        Button: "src/components/Button.ts",
+      },
       output: {
         globals: {
           react: "React",
           "react-dom": "ReactDOM",
+        },
+        inlineDynamicImports: false,
+        // eslint-disable-next-line consistent-return
+        manualChunks(id) {
+          if (id.includes("Accordion")) {
+            return "chunk/Accordion";
+          }
+          if (id.includes("Button")) {
+            return "Button";
+          }
         },
       },
     },
