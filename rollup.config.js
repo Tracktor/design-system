f56ef59045f8b5342a57ee40db420254b1f18552
mickaelaustoni <mickael.austoni@gmail.com>
2022-07-24 17:56:28 +0200

    config

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
+import external from "rollup-plugin-peer-deps-external";
 import postcss from "rollup-plugin-postcss";
 import { terser } from "rollup-plugin-terser";
 import { getFiles } from "./scripts/buildUtils";
@@ -19,6 +20,7 @@ export default {
   plugins: [
     resolve(),
     commonjs(),
+    external(),
     terser(),
     postcss({ modules: true }),
     typescript({
