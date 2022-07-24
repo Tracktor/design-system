5912729742fbaa8b769781dbbabdb3208ab3bbd6
mickaelaustoni <mickael.austoni@gmail.com>
2022-07-24 20:39:46 +0200

    config

--- rollup.config.js
+++ rollup.config.js
@@ -34,7 +34,7 @@ export default {
     dir: "dist",
     format: "esm",
     preserveModules: true,
-    // preserveModulesRoot: "src",
+    preserveModulesRoot: "src",
   },
   plugins: [
     external(),
