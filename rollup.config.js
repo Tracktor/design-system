c6b36795962758d45d138b9407ab72d4cdfeebe5
mickaelaustoni <mickael.austoni@gmail.com>
2022-07-24 18:57:13 +0200

    config

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,35 @@
+import fs from "fs";
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import external from "rollup-plugin-peer-deps-external";
 import postcss from "rollup-plugin-postcss";
 import { terser } from "rollup-plugin-terser";
-import { getFiles } from "./scripts/getFiles";
 
-const extensions = [".js", ".ts", ".jsx", ".tsx"];
+const getFiles = (entry, extensions = [], excludeExtensions = []) => {
+  let fileNames = [];
+  const dirs = fs.readdirSync(entry);
+
+  dirs.forEach((dir) => {
+    const path = `${entry}/${dir}`;
+
+    if (fs.lstatSync(path).isDirectory()) {
+      fileNames = [...fileNames, ...getFiles(path, extensions, excludeExtensions)];
+
+      return;
+    }
+
+    if (!excludeExtensions.some((exclude) => dir.endsWith(exclude)) && extensions.some((ext) => dir.endsWith(ext))) {
+      fileNames.push(path);
+    }
+  });
+
+  return fileNames;
+};
 
 export default {
   external: ["react", "react-dom"],
-  input: ["./src/index.ts", ...getFiles("./src/components", extensions)],
+  input: ["./src/index.ts", ...getFiles("./src/components", [".ts", ".tsx"])],
   output: {
     dir: "dist",
     format: "esm",
