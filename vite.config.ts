749dcb12b79fb0396d4f7ebcb24aa7c626aec2cf
mickaelaustoni <mickael.austoni@gmail.com>
2022-07-26 12:54:00 +0200

    test

--- vite.config.ts
+++ vite.config.ts
@@ -28,6 +28,9 @@ export default defineConfig({
         preserveModulesRoot: "src",
       },
     },
+    terserOptions: {
+      compress: true,
+    },
   },
   plugins: [react(), dts()],
   resolve: {
