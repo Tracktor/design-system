ab79dbde06a9f1452d6e58a3004fcd07d5d6daed
mickaelaustoni <mickael.austoni@gmail.com>
2022-07-27 16:35:45 +0200

    config

--- vite.config.ts
+++ vite.config.ts
@@ -23,18 +23,6 @@ export default defineConfig({
         },
         preserveModules: true,
         preserveModulesRoot: "src",
-        // manualChunks: (id, GetManualChunkApi) => {
-        //   console.log(GetManualChunkApi);
-        //
-        //   if (id.includes("node_modules")) {
-        //     if (id.includes("@mui/material")) return "vendor_mui_material";
-        //     if (id.includes("@mui/icons-material")) return "vendor_mui_icons_material";
-        //     if (id.includes("@emotion/styled")) return "vendor_emotion_styled";
-        //     if (id.includes("@emotion/styled")) return "vendor_emotion_react";
-        //     return "vendor";
-        //   }
-        //   return null;
-        // },
       },
     },
   },
