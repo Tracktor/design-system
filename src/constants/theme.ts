108a527d12db5da96402c065ff13d3ff75ca1aba
mickaelaustoni <mickael.austoni@gmail.com>
2022-07-29 17:19:48 +0200

    theme button

--- src/constants/theme.ts
+++ src/constants/theme.ts
@@ -1,27 +1,50 @@
 import { createTheme } from "@mui/material/styles";
+import type { ThemeOptions } from "@mui/material/styles/createTheme";
 import colors from "@/styles/colors.module.scss";
 
-const primary = {
-  main: colors.blue500,
+const components: ThemeOptions["components"] = {
+  MuiButton: {
+    styleOverrides: {
+      root: ({ ownerState }) => ({
+        borderRadius: 3,
+        ...(ownerState.color === "secondary" && {
+          "&:hover": {
+            backgroundColor: colors.yellow500,
+            borderColor: colors.yellow500,
+            color: colors.black,
+          },
+          backgroundColor: colors.yellow500,
+          borderColor: colors.yellow500,
+          color: colors.dark,
+        }),
+      }),
+    },
+  },
 };
 
 export const lightTheme = createTheme({
+  components,
   palette: {
     background: {
       default: colors.white,
     },
     mode: "light",
-    primary,
+    primary: {
+      main: colors.blue500,
+    },
   },
 });
 
 export const darkTheme = createTheme({
+  components,
   palette: {
     background: {
       default: colors.black,
     },
     mode: "dark",
-    primary,
+    primary: {
+      main: colors.blue500,
+    },
   },
 });
 
