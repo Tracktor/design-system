2137a4b80269b9b6bcb34000b233b4ef360aab61
mickaelaustoni <mickael.austoni@gmail.com>
2022-07-29 09:32:25 +0200

    Test export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 export * from "@mui/material";
 
+export { default as Box } from "@/components/Box";
+export * from "@/components/Box";
+
 export { default as ThemeProvider } from "@/context/Theme/ThemeProvider";
