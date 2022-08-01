991beeec07a44df4536212f9aa5f32e0b4faeeda
mickaelaustoni <mickael.austoni@gmail.com>
2022-08-01 13:41:17 +0200

    Box story

---
+++ src/stories/components/Layout/Box/Box.stories.tsx
@@ -0,0 +1,37 @@
+import { Stack } from "@mui/material";
+import type { ComponentStory, ComponentMeta } from "@storybook/react";
+import Box from "./Box";
+
+const Template: ComponentStory<typeof Box> = (args) => (
+  <Stack direction="row" spacing={2} alignItems="center" justifyContent="center" sx={{ height: "100%", width: "100%" }}>
+    <Box
+      sx={{
+        "&:hover": {
+          backgroundColor: "primary.main",
+          opacity: [0.9, 0.8, 0.7],
+        },
+        backgroundColor: "primary.dark",
+        height: 150,
+        width: 150,
+      }}
+      {...args}
+    />
+  </Stack>
+);
+
+const TemplateDashed: ComponentStory<typeof Box> = (args) => (
+  <Stack direction="row" spacing={2} alignItems="center" justifyContent="center" sx={{ height: "100%", width: "100%" }}>
+    <Box component="span" sx={{ border: "1px dashed grey", height: 150, width: 150 }} {...args} />
+  </Stack>
+);
+
+export const PrimaryBackground = Template.bind({});
+PrimaryBackground.args = {};
+
+export const Dashed = TemplateDashed.bind({});
+Dashed.args = {};
+
+export default {
+  component: Box,
+  title: "Components/Layout/Box",
+} as ComponentMeta<typeof Box>;
---
+++ src/stories/components/Layout/Box/Box.tsx
@@ -0,0 +1,5 @@
+import BoxMui, { BoxProps } from "@mui/material/Box";
+
+const Box = (props: BoxProps) => <BoxMui {...props} />;
+
+export default Box;
