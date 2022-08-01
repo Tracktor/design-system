515f17a6453e95cc75639ee7f3062426c26eede3
mickaelaustoni <mickael.austoni@gmail.com>
2022-08-01 17:03:38 +0200

    refactor : export named

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 export * from "@mui/material";
 
 // Components
-export { default as ABTest } from "@/components/IsFeatureEnable";
+export { default as IsFeatureEnable } from "@/components/IsFeatureEnable";
 
 // Context
 export { default as ThemeProvider } from "@/context/Theme/ThemeProvider";
 
 // Hooks
-export { default as useUserHasABTest } from "@/hooks/useIsFeatureEnabled";
+export { default as useIsFeatureEnabled } from "@/hooks/useIsFeatureEnabled";
