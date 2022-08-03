26024dcf94722a635b46ba52252a01616cd79c29
mickaelaustoni <mickael.austoni@gmail.com>
2022-08-03 16:06:02 +0200

    move IfFeatureEnable

---
+++ src/components/Layout/IfFeatureEnable/stories/IfFeatureEnable.tsx
@@ -0,0 +1,5 @@
+import IfFeatureEnableComponent, { IfFeatureEnableProps } from "../IfFeatureEnable";
+
+const IfFeatureEnable = (props: IfFeatureEnableProps) => <IfFeatureEnableComponent {...props} />;
+
+export default IfFeatureEnable;
