1bbed1c0fac4c7ef2d8e4259dcf2dfcbe93bade3
mickaelaustoni <mickael.austoni@gmail.com>
2022-08-04 13:54:36 +0200

    Slider story

---
+++ src/components/Inputs/Slider/stories/Slider.stories.tsx
@@ -0,0 +1,58 @@
+import { Box, Stack } from "@mui/material";
+import type { ComponentStory, ComponentMeta } from "@storybook/react";
+import { useState } from "react";
+import Slider from "./Slider";
+
+const Template: ComponentStory<typeof Slider> = (args) => (
+  <Stack spacing={2} alignItems="center" justifyContent="center" height="100%">
+    <Box width={250}>
+      <Slider defaultValue={30} aria-label="Disabled slider" {...args} />
+    </Box>
+    <Box width={250}>
+      <Slider disabled defaultValue={30} aria-label="Disabled slider" {...args} />
+    </Box>
+  </Stack>
+);
+
+const TemplateRange: ComponentStory<typeof Slider> = (args) => {
+  const [value, setValue] = useState<number[]>([20, 40]);
+
+  const handleChange = (_: Event, newValue: number | number[]) => {
+    setValue(newValue as number[]);
+  };
+
+  return (
+    <Stack spacing={2} alignItems="center" justifyContent="center" height="100%">
+      <Box width={250}>
+        <Slider defaultValue={30} aria-label="Disabled slider" value={value} onChange={handleChange} {...args} />
+      </Box>
+      <Box width={250}>
+        <Slider disabled defaultValue={30} aria-label="Disabled slider" value={[20, 40]} {...args} />
+      </Box>
+    </Stack>
+  );
+};
+export const ContinuousSliders = Template.bind({});
+ContinuousSliders.args = {};
+
+export const Small = Template.bind({});
+Small.args = {
+  size: "small",
+};
+
+export const DiscreteSliders = Template.bind({});
+DiscreteSliders.args = {
+  marks: true,
+  max: 100,
+  min: 10,
+  step: 10,
+  valueLabelDisplay: "auto",
+};
+
+export const RangeSliders = TemplateRange.bind({});
+RangeSliders.args = {};
+
+export default {
+  component: Slider,
+  title: "Components/Inputs/Slider",
+} as ComponentMeta<typeof Slider>;
---
+++ src/components/Inputs/Slider/stories/Slider.tsx
@@ -0,0 +1,5 @@
+import SliderMui, { SliderProps } from "@mui/material/Slider";
+
+const Select = (props: SliderProps) => <SliderMui {...props} />;
+
+export default Select;
