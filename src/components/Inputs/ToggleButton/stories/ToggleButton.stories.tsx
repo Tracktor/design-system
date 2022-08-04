acee1ac63b114efa7aa77a6fd0f778a73b822d3d
mickaelaustoni <mickael.austoni@gmail.com>
2022-08-04 14:31:01 +0200

    ToggleButton story

---
+++ src/components/Inputs/ToggleButton/stories/ToggleButton.stories.tsx
@@ -0,0 +1,63 @@
+import { Stack, ToggleButtonGroup } from "@mui/material";
+import type { ComponentStory, ComponentMeta } from "@storybook/react";
+import { useState, MouseEvent } from "react";
+import ToggleButton from "./ToggleButton";
+
+const TemplateExclusive: ComponentStory<typeof ToggleButton> = (args) => {
+  const [value, setValue] = useState<string | null>("b");
+
+  const handleAlignment = (_: MouseEvent<HTMLElement>, newValue: string | null) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Stack direction="row" spacing={2} alignItems="center" justifyContent="center" height="100%">
+      <ToggleButtonGroup value={value} onChange={handleAlignment} aria-label="Tracktor" exclusive>
+        <ToggleButton {...args} value="a" onChange={handleAlignment}>
+          Tracktor
+        </ToggleButton>
+        <ToggleButton {...args} value="b" onChange={handleAlignment}>
+          is
+        </ToggleButton>
+        <ToggleButton {...args} value="c" onChange={handleAlignment}>
+          awesome
+        </ToggleButton>
+      </ToggleButtonGroup>
+    </Stack>
+  );
+};
+
+const TemplateMultiple: ComponentStory<typeof ToggleButton> = (args) => {
+  const [value, setValue] = useState<string[] | null>(() => ["a", "c"]);
+
+  const handleAlignment = (_: MouseEvent<HTMLElement>, newValue: string[] | null) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Stack direction="row" spacing={2} alignItems="center" justifyContent="center" height="100%">
+      <ToggleButtonGroup value={value} onChange={handleAlignment} aria-label="Tracktor">
+        <ToggleButton {...args} value="a" onChange={handleAlignment}>
+          Tracktor
+        </ToggleButton>
+        <ToggleButton {...args} value="b" onChange={handleAlignment}>
+          is
+        </ToggleButton>
+        <ToggleButton {...args} value="c" onChange={handleAlignment}>
+          awesome
+        </ToggleButton>
+      </ToggleButtonGroup>
+    </Stack>
+  );
+};
+
+export const ExclusiveSelection = TemplateExclusive.bind({});
+ExclusiveSelection.args = {};
+
+export const MultipleSelection = TemplateMultiple.bind({});
+MultipleSelection.args = {};
+
+export default {
+  component: ToggleButton,
+  title: "Components/Inputs/ToggleButton",
+} as ComponentMeta<typeof ToggleButton>;
---
+++ src/components/Inputs/ToggleButton/stories/ToggleButton.tsx
@@ -0,0 +1,5 @@
+import ToggleButtonMui, { ToggleButtonProps } from "@mui/material/ToggleButton";
+
+const ToggleButton = (props: ToggleButtonProps) => <ToggleButtonMui {...props} />;
+
+export default ToggleButton;
