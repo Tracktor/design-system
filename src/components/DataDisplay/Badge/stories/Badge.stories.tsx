fa37fb09f4ca3621217554e8e4f53ee8d6894fa1
mickaelaustoni <mickael.austoni@gmail.com>
2022-08-11 11:21:13 +0200

    badge story

---
+++ src/components/DataDisplay/Badge/stories/Badge.stories.tsx
@@ -0,0 +1,91 @@
+import { Box, Stack, Typography } from "@mui/material";
+import type { ComponentStory, ComponentMeta } from "@storybook/react";
+import Badge from "./Badge";
+
+const Template: ComponentStory<typeof Badge> = (args) => (
+  <Stack direction="row" spacing={2} alignItems="center" justifyContent="center" height="100%">
+    <Badge badgeContent={4} {...args}>
+      <Typography variant="h4">✉️</Typography>
+    </Badge>
+  </Stack>
+);
+
+const TemplateCircle: ComponentStory<typeof Badge> = (args) => (
+  <Stack direction="row" spacing={2} alignItems="center" justifyContent="center" height="100%">
+    <Badge {...args}>
+      <Box component="span" sx={{ backgroundColor: "secondary.main", borderRadius: "50%", height: 40, width: 40 }} />
+    </Badge>
+  </Stack>
+);
+
+export const Basic = Template.bind({});
+Basic.args = {
+  badgeContent: 4,
+  color: "primary",
+};
+
+export const Color = Template.bind({});
+Color.args = {
+  badgeContent: 13,
+  color: "secondary",
+};
+
+export const Zero = Template.bind({});
+Zero.args = {
+  badgeContent: 0,
+  color: "primary",
+  showZero: true,
+};
+
+export const ZeroHidden = Template.bind({});
+ZeroHidden.args = {
+  badgeContent: 0,
+  color: "primary",
+  showZero: false,
+};
+
+export const Max = Template.bind({});
+Max.args = {
+  badgeContent: 1000,
+  color: "primary",
+  max: 99,
+};
+
+export const Dot = Template.bind({});
+Dot.args = {
+  color: "primary",
+  variant: "dot",
+};
+
+export const Alignment = Template.bind({});
+Alignment.args = {
+  anchorOrigin: { horizontal: "right", vertical: "top" },
+  color: "primary",
+};
+
+export const BadgeOverlap = TemplateCircle.bind({});
+BadgeOverlap.args = {
+  badgeContent: " ",
+  color: "primary",
+  overlap: "circular",
+};
+
+export default {
+  argTypes: {
+    anchorOrigin: {
+      mapping: {
+        BottomLeft: { horizontal: "left", vertical: "bottom" },
+        BottomRight: { horizontal: "right", vertical: "bottom" },
+        TopLeft: { horizontal: "left", vertical: "top" },
+        TopRight: { horizontal: "right", vertical: "top" },
+      },
+      options: ["BottomLeft", "BottomRight", "TopLeft", "TopRight"],
+    },
+    overlap: {
+      control: { type: "radio" },
+      options: ["circular", "rectangular"],
+    },
+  },
+  component: Badge,
+  title: "Components/Data Display/Badge",
+} as ComponentMeta<typeof Badge>;
---
+++ src/components/DataDisplay/Badge/stories/Badge.tsx
@@ -0,0 +1,5 @@
+import { Badge as Component, BadgeProps } from "@mui/material";
+
+export const Badge = (props: BadgeProps) => <Component {...props} />;
+
+export default Badge;
