e3f87f4b11e8549ab78350f16bf18a8cf20f142e
mickaelaustoni <mickael.austoni@gmail.com>
2022-08-16 14:51:57 +0200

    Backdrop stories

---
+++ src/components/Feedback/Backdrop/stories/Alert.stories.tsx
@@ -0,0 +1,33 @@
+import { Button, CircularProgress, Stack } from "@mui/material";
+import type { ComponentMeta, ComponentStory } from "@storybook/react";
+import { useState } from "react";
+import Backdrop from "./Backdrop";
+
+const Template: ComponentStory<typeof Backdrop> = (args) => {
+  const [open, setOpen] = useState(false);
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleToggle = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <Stack spacing={2} justifyContent="center" alignItems="center" sx={{ height: "100%" }}>
+      <Button onClick={handleToggle} variant="contained">
+        Click to show backdrop
+      </Button>
+      <Backdrop {...args} sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }} open={open} onClick={handleClose}>
+        <CircularProgress color="inherit" />
+      </Backdrop>
+    </Stack>
+  );
+};
+
+export const Basic = Template.bind({});
+Basic.args = {};
+
+export default {
+  component: Backdrop,
+  title: "Components/Feedback/Backdrop",
+} as ComponentMeta<typeof Backdrop>;
---
+++ src/components/Feedback/Backdrop/stories/Backdrop.tsx
@@ -0,0 +1,5 @@
+import { Backdrop as Component, BackdropProps } from "@mui/material";
+
+const Backdrop = (props: BackdropProps) => <Component {...props} />;
+
+export default Backdrop;
