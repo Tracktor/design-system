a7f29d4fd0e590074e8c36029b641cb7fef8d728
mickaelaustoni <mickael.austoni@gmail.com>
2022-08-17 09:14:56 +0200

    improve accordion stories

--- src/components/Surface/Accordion/stories/Accordion.stories.tsx
+++ src/components/Surface/Accordion/stories/Accordion.stories.tsx
@@ -1,11 +1,25 @@
-import { AccordionDetails, AccordionSummary, Stack, Typography } from "@mui/material";
-import type { ComponentStory, ComponentMeta } from "@storybook/react";
+import { AccordionDetails, AccordionSummary, Stack, Typography, useTheme } from "@mui/material";
+import type { ComponentMeta, ComponentStory } from "@storybook/react";
 import Accordion from "./Accordion";
 
+const ArrowIcon = () => {
+  const { palette } = useTheme();
+
+  return (
+    <svg width="20" height="20" x="0" y="0" viewBox="0 0 128 128">
+      <g>
+        <path
+          d="m64 88c-1.023 0-2.047-.391-2.828-1.172l-40-40c-1.563-1.563-1.563-4.094 0-5.656s4.094-1.563 5.656 0l37.172 37.172 37.172-37.172c1.563-1.563 4.094-1.563 5.656 0s1.563 4.094 0 5.656l-40 40c-.781.781-1.805 1.172-2.828 1.172z"
+          fill={palette.text.primary}
+        />
+      </g>
+    </svg>
+  );
+};
 const Template: ComponentStory<typeof Accordion> = (args) => (
   <Stack spacing={0} alignItems="stretch" justifyContent="center" height="100%">
     <Accordion {...args}>
-      <AccordionSummary expandIcon="+" aria-controls="panel1a-content" id="panel1a-header">
+      <AccordionSummary expandIcon={<ArrowIcon />} aria-controls="panel1a-content" id="panel1a-header">
         <Typography>Accordion 1</Typography>
       </AccordionSummary>
       <AccordionDetails>
@@ -15,7 +29,7 @@ const Template: ComponentStory<typeof Accordion> = (args) => (
       </AccordionDetails>
     </Accordion>
     <Accordion {...args}>
-      <AccordionSummary expandIcon="+" aria-controls="panel2a-content" id="panel2a-header">
+      <AccordionSummary expandIcon={<ArrowIcon />} aria-controls="panel2a-content" id="panel2a-header">
         <Typography>Accordion 2</Typography>
       </AccordionSummary>
       <AccordionDetails>
@@ -25,7 +39,7 @@ const Template: ComponentStory<typeof Accordion> = (args) => (
       </AccordionDetails>
     </Accordion>
     <Accordion disabled {...args}>
-      <AccordionSummary expandIcon="+" aria-controls="panel3a-content" id="panel3a-header">
+      <AccordionSummary expandIcon={<ArrowIcon />} aria-controls="panel3a-content" id="panel3a-header">
         <Typography>Disabled Accordion</Typography>
       </AccordionSummary>
     </Accordion>
