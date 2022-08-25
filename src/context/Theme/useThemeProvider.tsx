e19c4615a580e388a209b71abdeddd49769f130e
mickaelaustoni <mickael.austoni@gmail.com>
2022-08-25 17:30:13 +0200

    refactor: add type return useThemeProvider

--- src/context/Theme/useThemeProvider.tsx
+++ src/context/Theme/useThemeProvider.tsx
@@ -2,7 +2,7 @@ import { darkTheme, lightTheme } from "@/constants/theme";
 import type { ThemeProviderProps } from "@/context/Theme/ThemeProvider";
 
 const useThemeProvider = () => {
-  const getTheme = (theme: ThemeProviderProps["theme"]) => {
+  const getTheme = (theme: ThemeProviderProps["theme"]): Omit<ThemeProviderProps["theme"], "undefined"> => {
     if (theme === "dark") {
       return darkTheme;
     }
