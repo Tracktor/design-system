47399193d5cb7b819551089177249374ff07f0b2
mickaelaustoni <mickael.austoni@gmail.com>
2023-03-29 13:12:47 +0200

    build: improve colors package type entry

--- colors/index.d.ts
+++ colors/index.d.ts
@@ -1,4 +1,1 @@
-// This file is a type resolution module for TypeScript.
-// It is used for importing the module colors from the root directory.
-// Like this: import { red } from "@tracktor/design-system/colors";
-export * from "../dist/colors.js";
+export * from "../dist/colors/main";
