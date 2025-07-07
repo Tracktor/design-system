36f84c8e32b1c080d6fec2e40c917617628ed5c5
mickaelaustoni <mickael.austoni@gmail.com>
2025-07-07 14:33:24 +0200

    fix: build external dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,13 @@ import react from "@vitejs/plugin-react";
 import { defineConfig, UserConfig as UserConfigVite } from "vite";
 import dts from "vite-plugin-dts";
 import { UserConfig as InlineConfigVitest } from "vitest/config";
-import { name, peerDependencies } from "./package.json";
+import { dependencies, name, peerDependencies } from "./package.json";
 
 type UserConfig = UserConfigVite & {
   test: InlineConfigVitest["test"];
 };
 
-const allDependencies = [...Object.keys(peerDependencies), ...Object.keys(peerDependencies)];
+const allDependencies = [...Object.keys(dependencies), ...Object.keys(peerDependencies)];
 const external = Array.from(new Set(allDependencies));
 
 const config: UserConfig = {
