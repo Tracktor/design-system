ac7b8deae4826999e3ce25045c8720637212af49
kevin <kevin.graff1@gmail.com>
2025-10-02 09:57:26 +0200

    feat(Kanban): add storybook examples for single and multiple column layouts

--- src/components/DataDisplay/Kanban/stories/Kanban.stories.tsx
+++ src/components/DataDisplay/Kanban/stories/Kanban.stories.tsx
@@ -8,6 +8,87 @@ const Template: StoryFn<typeof Kanban> = (args) => (
   </Stack>
 );
 
+export const SingleColumnSingleItem = Template.bind({});
+SingleColumnSingleItem.args = {
+  data: [
+    {
+      count: 1,
+      isFetched: true,
+      items: [
+        {
+          id: "1",
+          image: undefined,
+          tag: "Task",
+          title: "My first task",
+        },
+      ],
+      label: "To Do",
+      name: "todo",
+    },
+  ],
+};
+
+export const TwoColumnsMinimal = Template.bind({});
+TwoColumnsMinimal.args = {
+  data: [
+    {
+      count: 1,
+      isFetched: true,
+      items: [
+        {
+          id: "1",
+          image: undefined,
+          tag: "Task",
+          title: "Task A",
+        },
+      ],
+      label: "To Do",
+      name: "todo",
+    },
+    {
+      count: 1,
+      isFetched: true,
+      items: [
+        {
+          id: "2",
+          image: undefined,
+          tag: "Task",
+          title: "Task B",
+        },
+      ],
+      label: "Done",
+      name: "done",
+    },
+  ],
+};
+
+export const ColumnEmptyButNotAll = Template.bind({});
+ColumnEmptyButNotAll.args = {
+  data: [
+    {
+      count: 0,
+      isFetched: true,
+      items: [],
+      label: "To Do",
+      name: "todo",
+    },
+    {
+      count: 1,
+      isFetched: true,
+      items: [
+        {
+          id: "3",
+          image: undefined,
+          tag: "Task",
+          title: "Working on it",
+        },
+      ],
+      label: "In Progress",
+      name: "inprogress",
+    },
+  ],
+};
+
 export const EmptyKanban = Template.bind({});
 EmptyKanban.args = {
   data: [],
