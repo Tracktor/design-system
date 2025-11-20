88dc2f67fc3f32e539b53258b15bcaa22bf430c7
MickaelAustoni <MickaelAustoni@users.noreply.github.com>
2025-11-20 12:35:48 +0000

    deploy: de0a532d48b6d902b53118a04ebcd5a4dcdaeb0d

---
+++ assets/AppBar.stories-CRZf8N8V.js
@@ -0,0 +1,24 @@
+import{a as r}from"./iframe-DXNqPFtc.js";import{C as a}from"./CloseIcon-CMydBvWp.js";import{A as n}from"./AppBar-vUY8vNpM.js";import{S as p}from"./Stack-WGB4i12r.js";import"./preload-helper-D9Z9MdNV.js";import"./Avatar-HJLbGB3x.js";import"./NavigationMenu-B4dHs66g.js";import"./Typography-D9hWEu3O.js";import"./Box-CnK9PWzc.js";import"./Divider-Bvsa0HAS.js";import"./dividerClasses-B-oM9jfb.js";import"./Fade-CPaCz6us.js";import"./List-DsJGAn_i.js";import"./ListItem-DJhwqpX6.js";import"./isMuiElement-8ZhNGszH.js";import"./Chip-Bm64OOGg.js";import"./useThemeProps-CrWXHhEN.js";import"./Drawer-CfGK51aX.js";import"./mergeSlotProps-DzOmsiQv.js";import"./Slide-DoNDM8U4.js";import"./debounce-Be36O1Ab.js";import"./ownerWindow-0wh9ozhh.js";import"./Modal-DJ5y3kod.js";import"./getScrollbarSize-CaCM53D3.js";import"./createChainedFunction-BO_9K8Jh.js";import"./Portal-D9TDajiT.js";import"./Backdrop-QqsoXLy3.js";import"./NoSsr-DbLr8A1S.js";import"./Toolbar-B-geLLeD.js";import"./Logo-HD7SE0Ge.js";import"./Skeleton-BpdSgVzT.js";import"./Button-D7peyyVs.js";import"./InputAdornment-BbxQQHUP.js";import"./useFormControl-BXLMYfnQ.js";import"./TextField-C2WTirDo.js";import"./Select-BoObg56F.js";import"./Menu-C9_GiYoh.js";import"./useSlotProps-C_0a8c92.js";import"./Popover-CXwufkfm.js";import"./MenuList-4qNFqKWU.js";import"./FormControl-DAtgmdUB.js";import"./useControlled-DbftVL_l.js";import"./formControlState-Dq1zat_P.js";import"./TextareaAutosize-Ctmra1hz.js";import"./InputLabel-BWzKRHQl.js";import"./FormLabel-DeFTw3dW.js";import"./useMenu-BeIyuGG-.js";import"./MenuItem-DAGV0PuK.js";import"./listItemIconClasses-CH1QC1mW.js";import"./ListItemText-Bgog2hqg.js";import"./ListItemIcon-B1rR7mrq.js";const c=[{icon:r(a,{}),label:"Check",url:"#"},{active:!0,icon:r(a,{}),label:"Close",url:"#"}],s=e=>r(p,{direction:"row",spacing:2,alignItems:"center",justifyContent:"center",sx:{height:"100%",width:"100%"},children:r(n,{actionProps:{children:"Action"},searchProps:{placeholder:"Search"},...e})}),m=e=>r(p,{direction:"row",spacing:2,alignItems:"center",justifyContent:"center",sx:{height:"100%",width:"100%"},children:r(n,{Search:null,Avatar:null,Action:null,...e})}),t=s.bind({});t.args={};const o=s.bind({});o.args={avatarProps:{menuItems:c}};const i=m.bind({});i.args={};const sr={component:n,title:"Components/Surface/AppBar"};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`args => <Stack direction="row" spacing={2} alignItems="center" justifyContent="center" sx={{
+  height: "100%",
+  width: "100%"
+}}>
+    <AppBar actionProps={{
+    children: "Action"
+  }} searchProps={{
+    placeholder: "Search"
+  }} {...args} />
+  </Stack>`,...t.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`args => <Stack direction="row" spacing={2} alignItems="center" justifyContent="center" sx={{
+  height: "100%",
+  width: "100%"
+}}>
+    <AppBar actionProps={{
+    children: "Action"
+  }} searchProps={{
+    placeholder: "Search"
+  }} {...args} />
+  </Stack>`,...o.parameters?.docs?.source}}};i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`args => <Stack direction="row" spacing={2} alignItems="center" justifyContent="center" sx={{
+  height: "100%",
+  width: "100%"
+}}>
+    <AppBar Search={null} Avatar={null} Action={null} {...args} />
+  </Stack>`,...i.parameters?.docs?.source}}};const cr=["Basic","WithAvatarMenu","Simple"];export{t as Basic,i as Simple,o as WithAvatarMenu,cr as __namedExportsOrder,sr as default};
---
+++ assets/Divider.stories-CeQ09CpE.js
@@ -0,0 +1,43 @@
+import{a as e,b as c}from"./iframe-DXNqPFtc.js";import{D as l}from"./Divider-Bvsa0HAS.js";import{B as a}from"./Box-CnK9PWzc.js";import{S as p}from"./Stack-WGB4i12r.js";import{C as m}from"./Chip-Bm64OOGg.js";import{G as d}from"./Grid-eiokc2OB.js";import{T as u}from"./Typography-D9hWEu3O.js";import"./preload-helper-D9Z9MdNV.js";import"./dividerClasses-B-oM9jfb.js";import"./useThemeProps-CrWXHhEN.js";import"./isMuiElement-8ZhNGszH.js";const i=r=>e(l,{...r});try{i.displayName="Divider",i.__docgenInfo={description:"",displayName:"Divider",props:{component:{defaultValue:null,description:"",name:"component",required:!1,type:{name:"ElementType<any, keyof IntrinsicElements>"}}}}}catch{}const t=e(u,{variant:"body2",children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus id dignissim justo. Nulla ut facilisis ligula. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed malesuada lobortis pretium."}),x=r=>e(p,{direction:"row",spacing:2,alignItems:"center",justifyContent:"center",height:"100%",children:e(a,{width:"100%",children:e(i,{...r})})}),h=r=>e(a,{display:"flex",justifyContent:"center",children:c(a,{maxWidth:500,sx:{border:"solid 1px",borderColor:"divider",borderRadius:2,p:2},children:[t,e(i,{...r,children:"CENTER"}),t,e(i,{textAlign:"left",...r,children:"LEFT"}),t,e(i,{textAlign:"right",...r,children:"RIGHT"}),t,e(i,{...r,children:e(m,{label:"CHIP"})}),t]})}),g=r=>e(a,{display:"flex",justifyContent:"center",alignItems:"center",height:"100%",children:e(a,{maxWidth:500,sx:{border:"solid 1px",borderColor:"divider",borderRadius:2,p:2},children:c(d,{container:!0,children:[e(d,{size:"grow",children:t}),e(i,{orientation:"vertical",flexItem:!0,...r,children:"VERTICAL"}),e(d,{size:"grow",children:t})]})})}),n=x.bind({});n.args={};const o=h.bind({});o.args={};const s=g.bind({});s.args={};const G={component:i,title:"Components/Data Display/Divider"};n.parameters={...n.parameters,docs:{...n.parameters?.docs,source:{originalSource:`args => <Stack direction="row" spacing={2} alignItems="center" justifyContent="center" height="100%">
+    <Box width="100%">
+      <Divider {...args} />
+    </Box>
+  </Stack>`,...n.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`args => <Box display="flex" justifyContent="center">
+    <Box maxWidth={500} sx={{
+    border: "solid 1px",
+    borderColor: "divider",
+    borderRadius: 2,
+    p: 2
+  }}>
+      {content}
+      <Divider {...args}>CENTER</Divider>
+      {content}
+      <Divider textAlign="left" {...args}>
+        LEFT
+      </Divider>
+      {content}
+      <Divider textAlign="right" {...args}>
+        RIGHT
+      </Divider>
+      {content}
+      <Divider {...args}>
+        <Chip label="CHIP" />
+      </Divider>
+      {content}
+    </Box>
+  </Box>`,...o.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`args => <Box display="flex" justifyContent="center" alignItems="center" height="100%">
+    <Box maxWidth={500} sx={{
+    border: "solid 1px",
+    borderColor: "divider",
+    borderRadius: 2,
+    p: 2
+  }}>
+      <Grid container>
+        <Grid size="grow">{content}</Grid>
+        <Divider orientation="vertical" flexItem {...args}>
+          VERTICAL
+        </Divider>
+        <Grid size="grow">{content}</Grid>
+      </Grid>
+    </Box>
+  </Box>`,...s.parameters?.docs?.source}}};const R=["Basic","WithText","Vertical"];export{n as Basic,s as Vertical,o as WithText,R as __namedExportsOrder,G as default};
