8f62ab6f4cb3898693f8a5098f33e1723b32ec57
graffkevin <graffkevin@users.noreply.github.com>
2025-12-30 18:03:05 +0000

    deploy: 806ecaa2770f42802dcd6b2cdd9c3c06075f84cd

---
+++ assets/AppBar.stories-BnjiwgJF.js
@@ -0,0 +1,24 @@
+import{a as r}from"./iframe-B0VrJzOz.js";import{C as a}from"./CloseIcon-VVNOojBv.js";import{A as n}from"./AppBar-BI-bFC7v.js";import{S as p}from"./Stack-CFPJg5t0.js";import"./preload-helper-PPVm8Dsz.js";import"./Avatar-Bp4f94vg.js";import"./NavigationMenu-BV2i1qaU.js";import"./Typography-C1tUvi7g.js";import"./Box-DoR0EV56.js";import"./Divider-DcAHKWyN.js";import"./dividerClasses-CW4SjeCD.js";import"./Fade-lxcwg63e.js";import"./List-D2i3nXTt.js";import"./ListItem-DMGoH2P6.js";import"./isMuiElement-CPUzgfuC.js";import"./Chip-DeQ3HkpB.js";import"./useThemeProps-Dcy7x4-_.js";import"./Drawer-DGKp0k02.js";import"./mergeSlotProps-bz8leqO4.js";import"./Slide-Cpfq07fv.js";import"./debounce-Be36O1Ab.js";import"./ownerWindow-kDCVe17W.js";import"./Modal-DY61Vtr9.js";import"./getScrollbarSize-CaCM53D3.js";import"./createChainedFunction-BO_9K8Jh.js";import"./Portal-BMB1NegX.js";import"./Backdrop-BC-55s2C.js";import"./NoSsr-CEf3hc7v.js";import"./Toolbar-BnFmo4_6.js";import"./Logo-C3od4Bgt.js";import"./Skeleton-DQTNZ0Nh.js";import"./Button-DMUK65Sb.js";import"./InputAdornment-DDJid_bj.js";import"./useFormControl-CRVvTgys.js";import"./TextField-OgU86wHr.js";import"./Select-D81E4tQp.js";import"./Menu-DjKbnycg.js";import"./useSlotProps-CtQqPa2V.js";import"./Popover-D69E1biu.js";import"./MenuList-BtbqDBdL.js";import"./FormControl-C3QXubCc.js";import"./useControlled-Ci6Z7jcY.js";import"./formControlState-Dq1zat_P.js";import"./TextareaAutosize-D3V-0xq0.js";import"./InputLabel-BKMqhA2t.js";import"./FormLabel-B0THllY8.js";import"./useMenu-CcT3Gjnl.js";import"./MenuItem-CghyG5H_.js";import"./listItemIconClasses-DPWu_NKL.js";import"./ListItemText-CL87kMXe.js";import"./ListItemIcon-DXyxCq-j.js";const c=[{icon:r(a,{}),label:"Check",url:"#"},{active:!0,icon:r(a,{}),label:"Close",url:"#"}],s=e=>r(p,{direction:"row",spacing:2,alignItems:"center",justifyContent:"center",sx:{height:"100%",width:"100%"},children:r(n,{actionProps:{children:"Action"},searchProps:{placeholder:"Search"},...e})}),m=e=>r(p,{direction:"row",spacing:2,alignItems:"center",justifyContent:"center",sx:{height:"100%",width:"100%"},children:r(n,{Search:null,Avatar:null,Action:null,...e})}),t=s.bind({});t.args={};const o=s.bind({});o.args={avatarProps:{menuItems:c}};const i=m.bind({});i.args={};const sr={component:n,title:"Components/Surface/AppBar"};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`args => <Stack direction="row" spacing={2} alignItems="center" justifyContent="center" sx={{
+  height: "100%",
+  width: "100%"
+}}>
+    <AppBar actionProps={{
+    children: "Action"
+  }} searchProps={{
+    placeholder: "Search"
+  }} {...args} />
+  </Stack>`,...t.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`args => <Stack direction="row" spacing={2} alignItems="center" justifyContent="center" sx={{
+  height: "100%",
+  width: "100%"
+}}>
+    <AppBar actionProps={{
+    children: "Action"
+  }} searchProps={{
+    placeholder: "Search"
+  }} {...args} />
+  </Stack>`,...o.parameters?.docs?.source}}};i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`args => <Stack direction="row" spacing={2} alignItems="center" justifyContent="center" sx={{
+  height: "100%",
+  width: "100%"
+}}>
+    <AppBar Search={null} Avatar={null} Action={null} {...args} />
+  </Stack>`,...i.parameters?.docs?.source}}};const cr=["Basic","WithAvatarMenu","Simple"];export{t as Basic,i as Simple,o as WithAvatarMenu,cr as __namedExportsOrder,sr as default};
---
+++ assets/Select.stories-BVY97tKz.js
@@ -0,0 +1,177 @@
+import{a as e,r as v,b as a}from"./iframe-B0VrJzOz.js";import{S,O as M}from"./Select-D81E4tQp.js";import{F as h}from"./FormControl-C3QXubCc.js";import{I}from"./InputLabel-BKMqhA2t.js";import{M as t}from"./MenuItem-CghyG5H_.js";import{S as b}from"./Stack-CFPJg5t0.js";import{C as T}from"./Checkbox-CkEQaCKf.js";import{L as x}from"./ListItemText-CL87kMXe.js";import"./preload-helper-PPVm8Dsz.js";import"./Menu-DjKbnycg.js";import"./useSlotProps-CtQqPa2V.js";import"./Popover-D69E1biu.js";import"./ownerWindow-kDCVe17W.js";import"./debounce-Be36O1Ab.js";import"./mergeSlotProps-bz8leqO4.js";import"./Modal-DY61Vtr9.js";import"./getScrollbarSize-CaCM53D3.js";import"./createChainedFunction-BO_9K8Jh.js";import"./Portal-BMB1NegX.js";import"./Backdrop-BC-55s2C.js";import"./Fade-lxcwg63e.js";import"./MenuList-BtbqDBdL.js";import"./List-D2i3nXTt.js";import"./useControlled-Ci6Z7jcY.js";import"./formControlState-Dq1zat_P.js";import"./useFormControl-CRVvTgys.js";import"./TextareaAutosize-D3V-0xq0.js";import"./isMuiElement-CPUzgfuC.js";import"./FormLabel-B0THllY8.js";import"./listItemIconClasses-DPWu_NKL.js";import"./dividerClasses-CW4SjeCD.js";import"./useThemeProps-Dcy7x4-_.js";import"./SwitchBase-CuuaY_8p.js";import"./Typography-C1tUvi7g.js";const s=l=>e(S,{...l});try{s.displayName="Select",s.__docgenInfo={description:"",displayName:"Select",props:{ref:{defaultValue:null,description:"",name:"ref",required:!1,type:{name:"((instance: unknown) => void | (() => VoidOrUndefinedOnly)) | RefObject<unknown> | null"}}}}}catch{}const y=48,A=8,w={PaperProps:{style:{maxHeight:y*4.5+A,width:250}}},k=["Oliver Hansen","Van Henry","April Tucker","Ralph Hubbard","Omar Alexander","Carlos Abbott","Miriam Wagner","Bradley Wilkerson","Virginia Andrews","Kelly Snyder"],C=l=>{const[r,m]=v.useState(""),{variant:o}=l,n=i=>{m(i.target.value)};return a(b,{direction:"row",spacing:2,alignItems:"center",justifyContent:"center",height:"100%",children:[a(h,{sx:{width:150},children:[e(I,{id:"demo-simple-select-label-xsmall",size:"xSmall",variant:o,children:"Age (xSmall)"}),a(s,{labelId:"demo-simple-select-label-small",value:r,label:"Age (xSmall)",onChange:n,size:"xSmall",...l,children:[e(t,{value:10,children:"Ten"}),e(t,{value:20,children:"Twenty"}),e(t,{value:30,children:"Thirty"})]})]}),a(h,{sx:{width:200},children:[e(I,{id:"demo-simple-select-label-small",size:"small",variant:o,children:"Age (small)"}),a(s,{labelId:"demo-simple-select-label-small",value:r,label:"Age (small)",onChange:n,size:"small",...l,children:[e(t,{value:10,children:"Ten"}),e(t,{value:20,children:"Twenty"}),e(t,{value:30,children:"Thirty"})]})]}),a(h,{sx:{width:200},children:[e(I,{id:"demo-simple-select-label",variant:o,children:"Age"}),a(s,{labelId:"demo-simple-select-label",value:r,label:"Age",onChange:n,...l,children:[e(t,{value:10,children:"Ten"}),e(t,{value:20,children:"Twenty"}),e(t,{value:30,children:"Thirty"})]})]})]})},f=l=>{const[r,m]=v.useState("20");return e(b,{direction:"row",spacing:2,alignItems:"center",justifyContent:"center",height:"100%",children:e(h,{children:a(s,{size:"small",value:r,label:"Age",onChange:n=>{m(n.target.value)},...l,children:[e(t,{value:10,children:"Ten"}),e(t,{value:20,children:"Twenty"}),e(t,{value:30,children:"Thirty"})]})})})},F=l=>{const[r,m]=v.useState([]);return e(b,{direction:"row",spacing:2,alignItems:"center",justifyContent:"center",height:"100%",children:a(h,{sx:{m:1,width:300},children:[e(I,{id:"demo-multiple-checkbox-label",children:"Tag"}),e(s,{labelId:"demo-multiple-checkbox-label",id:"demo-multiple-checkbox",multiple:!0,value:r,onChange:n=>{const{target:{value:i}}=n;m(typeof i=="string"?i.split(","):i)},input:e(M,{label:"Tag"}),renderValue:n=>n.join(", "),MenuProps:w,...l,children:k.map(n=>a(t,{value:n,children:[e(T,{checked:r.indexOf(n)>-1}),e(x,{primary:n})]},n))})]})})},c=C.bind({});c.args={variant:"outlined"};const u=C.bind({});u.args={variant:"filled"};const d=C.bind({});d.args={variant:"standard"};const g=f.bind({});g.args={disableUnderline:!0,variant:"standard"};const p=F.bind({});p.args={};const ce={component:s,title:"Components/Inputs/Select"};c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`args => {
+  const [age, setAge] = useState("");
+  const {
+    variant
+  } = args;
+  const handleChange = (event: SelectChangeEvent) => {
+    setAge(event.target.value as string);
+  };
+  return <Stack direction="row" spacing={2} alignItems="center" justifyContent="center" height="100%">
+      <FormControl sx={{
+      width: 150
+    }}>
+        <InputLabel id="demo-simple-select-label-xsmall" size="xSmall" variant={variant}>
+          Age (xSmall)
+        </InputLabel>
+        <Select labelId="demo-simple-select-label-small" value={age} label="Age (xSmall)" onChange={handleChange} size="xSmall" {...args}>
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
+      <FormControl sx={{
+      width: 200
+    }}>
+        <InputLabel id="demo-simple-select-label-small" size="small" variant={variant}>
+          Age (small)
+        </InputLabel>
+        <Select labelId="demo-simple-select-label-small" value={age} label="Age (small)" onChange={handleChange} size="small" {...args}>
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
+      <FormControl sx={{
+      width: 200
+    }}>
+        <InputLabel id="demo-simple-select-label" variant={variant}>
+          Age
+        </InputLabel>
+        <Select labelId="demo-simple-select-label" value={age} label="Age" onChange={handleChange} {...args}>
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
+    </Stack>;
+}`,...c.parameters?.docs?.source}}};u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`args => {
+  const [age, setAge] = useState("");
+  const {
+    variant
+  } = args;
+  const handleChange = (event: SelectChangeEvent) => {
+    setAge(event.target.value as string);
+  };
+  return <Stack direction="row" spacing={2} alignItems="center" justifyContent="center" height="100%">
+      <FormControl sx={{
+      width: 150
+    }}>
+        <InputLabel id="demo-simple-select-label-xsmall" size="xSmall" variant={variant}>
+          Age (xSmall)
+        </InputLabel>
+        <Select labelId="demo-simple-select-label-small" value={age} label="Age (xSmall)" onChange={handleChange} size="xSmall" {...args}>
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
+      <FormControl sx={{
+      width: 200
+    }}>
+        <InputLabel id="demo-simple-select-label-small" size="small" variant={variant}>
+          Age (small)
+        </InputLabel>
+        <Select labelId="demo-simple-select-label-small" value={age} label="Age (small)" onChange={handleChange} size="small" {...args}>
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
+      <FormControl sx={{
+      width: 200
+    }}>
+        <InputLabel id="demo-simple-select-label" variant={variant}>
+          Age
+        </InputLabel>
+        <Select labelId="demo-simple-select-label" value={age} label="Age" onChange={handleChange} {...args}>
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
+    </Stack>;
+}`,...u.parameters?.docs?.source}}};d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`args => {
+  const [age, setAge] = useState("");
+  const {
+    variant
+  } = args;
+  const handleChange = (event: SelectChangeEvent) => {
+    setAge(event.target.value as string);
+  };
+  return <Stack direction="row" spacing={2} alignItems="center" justifyContent="center" height="100%">
+      <FormControl sx={{
+      width: 150
+    }}>
+        <InputLabel id="demo-simple-select-label-xsmall" size="xSmall" variant={variant}>
+          Age (xSmall)
+        </InputLabel>
+        <Select labelId="demo-simple-select-label-small" value={age} label="Age (xSmall)" onChange={handleChange} size="xSmall" {...args}>
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
+      <FormControl sx={{
+      width: 200
+    }}>
+        <InputLabel id="demo-simple-select-label-small" size="small" variant={variant}>
+          Age (small)
+        </InputLabel>
+        <Select labelId="demo-simple-select-label-small" value={age} label="Age (small)" onChange={handleChange} size="small" {...args}>
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
+      <FormControl sx={{
+      width: 200
+    }}>
+        <InputLabel id="demo-simple-select-label" variant={variant}>
+          Age
+        </InputLabel>
+        <Select labelId="demo-simple-select-label" value={age} label="Age" onChange={handleChange} {...args}>
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
+    </Stack>;
+}`,...d.parameters?.docs?.source}}};g.parameters={...g.parameters,docs:{...g.parameters?.docs,source:{originalSource:`args => {
+  const [age, setAge] = useState("20");
+  const handleChange = (event: SelectChangeEvent) => {
+    setAge(event.target.value as string);
+  };
+  return <Stack direction="row" spacing={2} alignItems="center" justifyContent="center" height="100%">
+      <FormControl>
+        <Select size="small" value={age} label="Age" onChange={handleChange} {...args}>
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
+    </Stack>;
+}`,...g.parameters?.docs?.source}}};p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`args => {
+  const [personName, setPersonName] = useState<string[]>([]);
+  const handleChange = (event: SelectChangeEvent<typeof personName>) => {
+    const {
+      target: {
+        value
+      }
+    } = event;
+    setPersonName(typeof value === "string" ? value.split(",") : value);
+  };
+  return <Stack direction="row" spacing={2} alignItems="center" justifyContent="center" height="100%">
+      <FormControl sx={{
+      m: 1,
+      width: 300
+    }}>
+        <InputLabel id="demo-multiple-checkbox-label">Tag</InputLabel>
+        <Select labelId="demo-multiple-checkbox-label" id="demo-multiple-checkbox" multiple value={personName} onChange={handleChange} input={<OutlinedInput label="Tag" />} renderValue={selected => selected.join(", ")} MenuProps={MenuProps} {...args}>
+          {names.map(name => <MenuItem key={name} value={name}>
+              <Checkbox checked={personName.indexOf(name) > -1} />
+              <ListItemText primary={name} />
+            </MenuItem>)}
+        </Select>
+      </FormControl>
+    </Stack>;
+}`,...p.parameters?.docs?.source}}};const ue=["Basic","Filled","Standard","Text","Checkmarks"];export{c as Basic,p as Checkmarks,u as Filled,d as Standard,g as Text,ue as __namedExportsOrder,ce as default};
