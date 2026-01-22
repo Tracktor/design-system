ec22e67cb2fd79acca4652eab8e5972eafb55bce
MickaelAustoni <MickaelAustoni@users.noreply.github.com>
2026-01-22 15:23:52 +0000

    deploy: d76f8dbdbd2d78473c558b498dbc227d8fcfc06e

---
+++ assets/AppBar.stories-ZDQmpBLb.js
@@ -0,0 +1,24 @@
+import{a as r}from"./iframe-CA_CZfNl.js";import{C as a}from"./CloseIcon-Ct8EBNCZ.js";import{A as n}from"./AppBar-Dq9h0QYD.js";import{S as p}from"./Stack-DPmfBFfu.js";import"./preload-helper-PPVm8Dsz.js";import"./Avatar-Bz4Dh2Ph.js";import"./NavigationMenu-IEBvMiNn.js";import"./Typography-_iZKJk8c.js";import"./Box-CiKI4V43.js";import"./Divider-Cat9Lwk4.js";import"./dividerClasses-o27n4VOQ.js";import"./Fade-DHHGFNLm.js";import"./List-CInnq3EZ.js";import"./ListItem-DQrdKdyq.js";import"./isMuiElement-C6R-BPvq.js";import"./Chip-ClvfwqeL.js";import"./useThemeProps-mw7ovPa8.js";import"./Drawer-B-wMK-PM.js";import"./mergeSlotProps-zm9Z8n-S.js";import"./Slide-Blia7smw.js";import"./debounce-Be36O1Ab.js";import"./ownerWindow-BrIYJ5T_.js";import"./Modal-DWR4uzCh.js";import"./getScrollbarSize-CaCM53D3.js";import"./createChainedFunction-BO_9K8Jh.js";import"./Portal-9tidJRNu.js";import"./Backdrop-Cj895AVh.js";import"./NoSsr-BPj5TlVp.js";import"./Toolbar-2xSlhmx_.js";import"./Logo-DjYLKka2.js";import"./Skeleton-BouAO1dl.js";import"./Button-KcMK-BSk.js";import"./InputAdornment-DfzF6Rpz.js";import"./useFormControl-B_ugBAqz.js";import"./TextField-CBfSGosU.js";import"./Select-DkYJUjTU.js";import"./Menu-DWLbAew_.js";import"./useSlotProps-oQKANgzm.js";import"./Popover-DbQOdJxF.js";import"./MenuList-tTfJtGv_.js";import"./FormControl-DtHk86bb.js";import"./useControlled-Ckets7tE.js";import"./formControlState-Dq1zat_P.js";import"./TextareaAutosize-CgERCsDY.js";import"./InputLabel-C_5JQ3dx.js";import"./FormLabel-Bom_IFIg.js";import"./useMenu-CA0i92xe.js";import"./MenuItem-BQ-nwASJ.js";import"./listItemIconClasses-BJ5hYqXU.js";import"./ListItemText-BYzq1csv.js";import"./ListItemIcon-BxfK7fiv.js";const c=[{icon:r(a,{}),label:"Check",url:"#"},{active:!0,icon:r(a,{}),label:"Close",url:"#"}],s=e=>r(p,{direction:"row",spacing:2,alignItems:"center",justifyContent:"center",sx:{height:"100%",width:"100%"},children:r(n,{actionProps:{children:"Action"},searchProps:{placeholder:"Search"},...e})}),m=e=>r(p,{direction:"row",spacing:2,alignItems:"center",justifyContent:"center",sx:{height:"100%",width:"100%"},children:r(n,{Search:null,Avatar:null,Action:null,...e})}),t=s.bind({});t.args={};const o=s.bind({});o.args={avatarProps:{menuItems:c}};const i=m.bind({});i.args={};const sr={component:n,title:"Components/Surface/AppBar"};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`args => <Stack direction="row" spacing={2} alignItems="center" justifyContent="center" sx={{
+  height: "100%",
+  width: "100%"
+}}>
+    <AppBar actionProps={{
+    children: "Action"
+  }} searchProps={{
+    placeholder: "Search"
+  }} {...args} />
+  </Stack>`,...t.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`args => <Stack direction="row" spacing={2} alignItems="center" justifyContent="center" sx={{
+  height: "100%",
+  width: "100%"
+}}>
+    <AppBar actionProps={{
+    children: "Action"
+  }} searchProps={{
+    placeholder: "Search"
+  }} {...args} />
+  </Stack>`,...o.parameters?.docs?.source}}};i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`args => <Stack direction="row" spacing={2} alignItems="center" justifyContent="center" sx={{
+  height: "100%",
+  width: "100%"
+}}>
+    <AppBar Search={null} Avatar={null} Action={null} {...args} />
+  </Stack>`,...i.parameters?.docs?.source}}};const cr=["Basic","WithAvatarMenu","Simple"];export{t as Basic,i as Simple,o as WithAvatarMenu,cr as __namedExportsOrder,sr as default};
--- assets/ownerWindow-DCr_4UwQ.js
+++
@@ -1,1 +0,0 @@
-import{v as n}from"./iframe-XxQpz1LY.js";function t(o){return n(o).defaultView||window}export{t as o};
